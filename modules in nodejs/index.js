9984b654065090f553e27ecd6d5f448544ee4e55
Saad Rahman <saadreh5@gmail.com>
2025-03-03 15:36:57 +0500

    modules in nodejs doen

---
+++ modules in nodejs/index.js
@@ -0,0 +1,9 @@
+// const { add, sub } = require("./math.js");
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+const math = require("./math");
+
+console.log(math);
+console.log(math.add(1, 2));
+console.log(math.sub(1, 2));
