fa6cad0ff750c00c2f007d0d0fcc0c7086c35003
Saad Rahman <saadreh5@gmail.com>
2025-03-04 21:08:48 +0500

    created the PATCH ROUTE

--- project-01-continued/index.js
+++ project-01-continued/index.js
@@ -31,15 +31,32 @@ app
     const user = users.find((user) => user.id === userId);
     return res.json(user);
   })
-  .patch((req, res) => {})
-  .delete((req, res) => {});
-app.get("/", (req, res) => {
-  return res.send("this is home page");
-});
+  .patch((req, res) => {
+    // TODO: Implement update user
+    const userId = Number(req.params.id);
+    const user = users.find((user) => user.id === userId);
 
-app.get("/about", (req, res) => {
-  return res.send(`This is about page, params = ${req.query.name}`);
-});
+    const updateData = req.body;
+    const updatedUser = { ...user, ...updateData };
+    const newUsers = users.map((user) => {
+      if (user.id === updatedUser.id) {
+        return updatedUser;
+      } else {
+        return user;
+      }
+    });
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(newUsers), (err) => {
+      if (err) {
+        return res.json({ status: err });
+      } else {
+        return res.json({ status: "success", user_updated: updatedUser });
+      }
+    });
+  })
+  .delete((req, res) => {
+    // TODO: Implement delete user by id
+  });
 
 app.listen(8000, () => {
   console.log("Listening on port 8000");
